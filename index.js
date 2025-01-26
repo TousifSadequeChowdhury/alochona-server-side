21088b23f09998b9d9ab6aad4c491c6f1f8df3bf
TousifsadequeChowdhury <tousifchowdhurybd@gmail.com>
2025-01-26 17:56:57 +0600

    Created a POST API endpoint to allow users to create new posts.

---
+++ index.js
@@ -0,0 +1,79 @@
+const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection URI
+const uri = "mongodb+srv://alochonaadmin:abcd1234@cluster0.8emp0.mongodb.net/AllPost?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connection successful"))
+  .catch((err) => console.log("Database connection error:", err));
+
+// Define a schema
+const postSchema = new mongoose.Schema({
+  authorImage: { type: String, required: true },
+  authorName: { type: String, required: true },
+  authorEmail: { type: String, required: true },
+  postTitle: { type: String, required: true },
+  postDescription: { type: String, required: true },
+  tag: { type: String, required: true },
+  upVote: { type: Number, required: true },
+  downVote: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+
+});
+
+// Create a model
+const Post = mongoose.model("Post", postSchema);
+
+// Create an API endpoint to handle POST requests
+app.post('/api/posts', async (req, res) => {
+  try {
+  const { 
+      postTitle, 
+      postDescription, 
+      tag, 
+      upVote, 
+      downVote, 
+      authorImage, 
+      authorName, 
+      authorEmail 
+  } = req.body;
+
+  if (!postTitle || !postDescription || !authorEmail || !authorName || !authorImage || upVote === undefined || downVote === undefined) {
+      return res.status(400).json({ error: "All fields are required." });
+  }
+
+
+ 
+      const newPost = new Post({
+          authorImage,
+          authorName,
+          authorEmail,
+          postTitle,
+          postDescription,
+          tag,
+          upVote,
+          downVote,
+      });
+
+        const savedPost = await newPost.save();
+      res.status(201).json(newPost);
+  } catch (err) {
+      res.status(500).json({ error: "Failed to create post." });
+  }
+});
+
+
+
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log("App listening at port 3000");
+});
